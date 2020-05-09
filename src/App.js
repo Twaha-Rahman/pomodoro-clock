d961e4a825d2503d2aaa2fa244ece9428607cbb9
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-09 15:21:21 +0600

    FCC ids added

--- src/App.js
+++ src/App.js
@@ -11,30 +11,44 @@ function App() {
         <div className="controller-container">
           <h3 id="break-label">Break Length</h3>
           <div className="setter-container">
-            <i class="fas fa-arrow-up"></i>
-            <strong>5</strong>
-            <i class="fas fa-arrow-down"></i>
+            <button id="break-increment">
+              <i class="fas fa-arrow-up"></i>
+            </button>
+
+            <strong id="break-length">5</strong>
+            <button id="break-decrement">
+              <i class="fas fa-arrow-down"></i>
+            </button>
           </div>
         </div>
 
         <div className="controller-container">
           <h3 id="session-label">Session Length</h3>
           <div className="setter-container">
-            <i class="fas fa-arrow-up"></i>
-            <strong>25</strong>
-            <i class="fas fa-arrow-down"></i>
+            <button id="session-increment">
+              <i class="fas fa-arrow-up"></i>
+            </button>
+
+            <strong id="session-length">25</strong>
+            <button id="session-decrement">
+              <i class="fas fa-arrow-down"></i>
+            </button>
           </div>
         </div>
       </div>
 
       <div className="timer-container">
         <h2 id="timer-label">Session</h2>
-        <h1>1:00</h1>
+        <h1 id="time-left">1:00</h1>
       </div>
 
       <div className="timing-controlls">
-        <i class="fas fa-pause"></i>
-        <i class="fas fa-sync-alt"></i>
+        <button id="start_stop">
+          <i class="fas fa-pause"></i>
+        </button>
+        <button id="reset">
+          <i class="fas fa-sync-alt"></i>
+        </button>
       </div>
     </div>
   );
