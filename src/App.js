87cafb9dff02ddcb168c1d4460238b32f825cfbf
Twaha-Rahman <mahi6703890@gmail.com>
2020-05-09 10:36:58 +0600

    Added the HTML for both break-control and session-control

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="clock-container">
+      <h1>Pomodoro Clock</h1>
+      <div className="break-container">
+        <h4 className="break-label">Break Length</h4>
+        <div className="break-setter-container">
+          <strong>+</strong>
+          <strong>5</strong>
+          <strong>-</strong>
+        </div>
+      </div>
+      <div className="session-container">
+        <h4 className="session-label">Break Length</h4>
+        <div className="session-setter-container">
+          <strong>+</strong>
+          <strong>25</strong>
+          <strong>-</strong>
+        </div>
+      </div>
     </div>
   );
 }
